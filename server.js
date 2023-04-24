201fc03f1b92ae4b7e52f9ee6d7c8fd0868afa4a
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-04-24 17:49:57 -0500

    some backend stuff

--- server.js
+++ server.js
@@ -4,15 +4,44 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+
+const Book = require('./models/books');
+
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
-app.get('/test', (request, response) => {
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
-  response.send('test request received')
+mongoose.connect(process.env.DB_URL);
 
-})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome');
+});
+
+app.get('/books', async (req, res, next) => {
+  try {
+    let allBooks = await Book.find({});
+    console.log(allBooks);
+    res.status(200).send(allBooks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not Available');
+});
+
+app.use((error, req, res, next) => {
+  console.log('error.message');
+  res.status(500).send(error.message);
+});
